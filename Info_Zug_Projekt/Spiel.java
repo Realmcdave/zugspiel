9e6871c1f649d4abe75f7f078724bc65135c14e6
sycrw <timmartinkausemann@gmail.com>
2023-06-27 22:35:16 +0200

    commit final

--- Info_Zug_Projekt/Spiel.java
+++ Info_Zug_Projekt/Spiel.java
@@ -22,7 +22,18 @@ public class Spiel {
         zugnetz = new Zugnetz(BahnhoefeZahl, breite, hoehe, leinwand);
         zuege = 0;
         // print Geschichte zur console
-
+        System.out.println("Richards Reisen:");
+        System.out.println(
+                "Richards Startpunkt ist der Punkt 0 und der rot markierte Punk ist der Ort an dem seine Freunde auf ihn warten.");
+        System.out.println(
+                "Die Nachbarstädte von Richard sind auch nummeriert, aber er weiß nicht welcher Ort welche Nummer besitzt und muss so durch Ausprobieren und dem Merken der Nummern seinen Weg finden.");
+        System.out.println(
+                "Du kannst Richard helfen in dem du eine Nummer in das Eingabefeld gibst. Wenn eine der verbundenen Nachbarstädte diese Nummer trägt fährt Richard in diese Stadt. Wenn nicht, dann musst du es mit einer anderen Nummer oder mit einer zufälligen Nummer (Eingabe: Zufall) versuchen.");
+        System.out.println("Wenn eine der verbundenen Nachbarstädte diese Nummer trägt fährt Richard in diese Stadt.");
+        System.out.println(
+                "Wenn nicht, dann musst du es mit einer anderen Nummer oder mit einer zufälligen Nummer (Eingabe: Zufall) versuchen.");
+        System.out.println("Euer Ziel ist wie bereits gesagt, dass Richard schnellstmöglich seine Freunde findet.");
+        System.out.println("Let's go");
         // starte game loop
         gameLoop();
     }
